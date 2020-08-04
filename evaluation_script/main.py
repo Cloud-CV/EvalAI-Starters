65cd8eabb40d41083452f71d9159dae0940260b4
VISHAL SINGH <56965195+vishalsingha@users.noreply.github.com>
2020-08-04 07:01:12 +0530

    Removed extra lines of code from main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,5 +1,13 @@
 import random
 
+def random_metric_generator():
+    return {
+            "Metric1": random.randint(0, 99),
+            "Metric2": random.randint(0, 99),
+            "Metric3": random.randint(0, 99),
+            "Total": random.randint(0, 99),
+           }
+    
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
     print("Starting Evaluation.....")
@@ -42,39 +50,18 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     output = {}
     if phase_codename == "dev":
         print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            }
-        ]
+        output["result"] = [{"train_split": random_metric_generator()}]
+        
         # To display the results in the result file
         output["submission_result"] = output["result"][0]["train_split"]
         print("Completed evaluation for Dev Phase")
     elif phase_codename == "test":
         print("Evaluating for Test Phase")
         output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-        ]
+                            {"train_split": random_metric_generator()},
+                            {"test_split": random_metric_generator()},
+                           ]
+        
         # To display the results in the result file
         output["submission_result"] = output["result"][0]
         print("Completed evaluation for Test Phase")

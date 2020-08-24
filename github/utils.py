19635f50db87f07f62deb6f4866cdf20baa11579
Khalid Riyaz <Khalidriyazgit@gmail.com>
2020-08-24 19:56:11 +0530

    Update utils.py

--- github/utils.py
+++ github/utils.py
@@ -33,6 +33,7 @@ def load_host_configs(config_path):
     """
     Loads token to be used for sending requests
     """
+    print(">>>>>>>>>>> Host config path is: {}".format(config_path))
     if os.path.exists(config_path):
         with open(config_path, "r") as f:
             try:
@@ -47,8 +48,9 @@ def load_host_configs(config_path):
         evalai_host_url = data["evalai_host_url"]
         return [host_auth_token, challenge_host_team_pk, evalai_host_url]
     else:
-        print("\nThe host config json file is not present. Please include an auth token, team_pk & evalai_host_url in it: {}".format(config_path))
-        os.environ["CHALLENGE_ERRORS"] = error
+        erro_message = "\nThe host config json file is not present. Please include an auth token, team_pk & evalai_host_url in it: {}".format(config_path)
+        print(error_message)
+        os.environ["CHALLENGE_ERRORS"] = error_message
         return False
 
 def validate_token(response):

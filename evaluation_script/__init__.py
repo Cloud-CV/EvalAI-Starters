4c041e2101140c6a0149bf2803a535f86519bbd9
Rishabh Jain <rishabhjain2018@gmail.com>
2021-01-25 01:34:22 -0500

    Add code for installing custom python-pip packages in evaluation script(#41)
    * Add header for staging and production
    
    * Install custom python packages

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -1,1 +1,17 @@
+"""
+Q. How to install custom python pip packages?
+
+A. Uncomment the below code to install the custom python packages.
+
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("shapely")
+install("requests")
+
+"""
+
 from .main import evaluate

f265209d8f1691fc32484d0f32dc7ebcafc1196b
RishabhJain2018 <rishabhjain2018@gmail.com>
2021-01-25 11:24:44 -0500

    Add code to install custom local package in evaluation script

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -1,16 +1,42 @@
 """
-Q. How to install custom python pip packages?
+# Q. How to install custom python pip packages?
 
-A. Uncomment the below code to install the custom python packages.
+# A. Uncomment the below code to install the custom python packages.
 
+import os
 import subprocess
 import sys
+from pathlib import Path
 
 def install(package):
+    # Install a pip python package
+
+    # Args:
+    #     package ([str]): Package name with version
+    
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install("shapely")
-install("requests")
+
+def install_local_package(folder_name):
+    # Install a local python package
+
+    # Args:
+    #     folder_name ([str]): name of the folder placed in evaluation_script/
+    
+    subprocess.check_output(
+    [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
+    ]
+)
+
+install("shapely==1.7.1")
+install("requests==2.25.1")
+
+install_local_package("package_folder_name")
 
 """
 

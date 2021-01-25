26fa33ebfe17de8c45feaa858606a2d0ab7c6ff6
RishabhJain2018 <rishabhjain2018@gmail.com>
2021-01-24 23:53:26 -0500

    Minor fix

--- github/config.py
+++ github/config.py
@@ -4,9 +4,7 @@ import os
 os.environ["CHALLENGE_ERRORS"] = "False"
 
 HOST_CONFIG_FILE_PATH = "github/host_config.json"
-CHALLENGE_CONFIG_VALIDATION_URL = (
-    "/api/challenges/challenge/challenge_host_team/{}/validate_challenge_config/"
-)
+CHALLENGE_CONFIG_VALIDATION_URL = "/api/challenges/challenge/challenge_host_team/{}/validate_challenge_config/"
 CHALLENGE_CREATE_OR_UPDATE_URL = "/api/challenges/challenge/challenge_host_team/{}/create_or_update_github_challenge/"
 EVALAI_ERROR_CODES = [400, 401, 406]
 API_HOST_URL = "https://eval.ai"

163f1ffc25fe925f26003c96028a11a2afa84bd4
RishabhJain2018 <rishabhjain2018@gmail.com>
2021-01-24 23:52:35 -0500

    Ignore folder while creating challenge using GitHub

--- github/config.py
+++ github/config.py
@@ -4,11 +4,19 @@ import os
 os.environ["CHALLENGE_ERRORS"] = "False"
 
 HOST_CONFIG_FILE_PATH = "github/host_config.json"
-CHALLENGE_CONFIG_VALIDATION_URL = "/api/challenges/challenge/challenge_host_team/{}/validate_challenge_config/"
+CHALLENGE_CONFIG_VALIDATION_URL = (
+    "/api/challenges/challenge/challenge_host_team/{}/validate_challenge_config/"
+)
 CHALLENGE_CREATE_OR_UPDATE_URL = "/api/challenges/challenge/challenge_host_team/{}/create_or_update_github_challenge/"
 EVALAI_ERROR_CODES = [400, 401, 406]
 API_HOST_URL = "https://eval.ai"
-IGNORE_DIRS = [".git", ".github", "github", "code_upload_challenge_evaluation"]
+IGNORE_DIRS = [
+    ".git",
+    ".github",
+    "github",
+    "code_upload_challenge_evaluation",
+    "remote_challenge_evaluation",
+]
 IGNORE_FILES = [
     ".gitignore",
     "challenge_config.zip",
--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ rm challenge_config.zip
 
 # Create new zip configuration according the updated code
 zip -r -j evaluation_script.zip evaluation_script/*  -x "*.DS_Store"
-zip -r challenge_config.zip *  -x "*.DS_Store" -x "evaluation_script/*" -x "*.git" -x "run.sh" -x "code_upload_challenge_evaluation/*" -x "worker/*" -x "challenge_data/*" -x "github/*" -x ".github/*" -x "README.md"
+zip -r challenge_config.zip *  -x "*.DS_Store" -x "evaluation_script/*" -x "*.git" -x "run.sh" -x "code_upload_challenge_evaluation/*" -x "remote_challenge_evaluation/*" -x "worker/*" -x "challenge_data/*" -x "github/*" -x ".github/*" -x "README.md"

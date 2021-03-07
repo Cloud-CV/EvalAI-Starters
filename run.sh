b37b44fed21c2fa4a515536bda5bc0472bf6e9b5
Yash Kant <ysh.kant@gmail.com>
2021-03-06 19:43:15 -0500

    Fix run.sh for creating evaluation script zip file(#48)
    Context: 
    
    Currently, the challenge creation using `challenge_config.zip` requires the `evaluation_script.zip` to be without the outer-level directory. This PR fixes it.

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ rm evaluation_script.zip
 rm challenge_config.zip
 
 # Create new zip configuration according the updated code
-zip -r -j evaluation_script.zip evaluation_script/*  -x "*.DS_Store"
+cd evaluation_script
+zip -r ../evaluation_script.zip * -x "*.DS_Store"
+cd ..
 zip -r challenge_config.zip *  -x "*.DS_Store" -x "evaluation_script/*" -x "*.git" -x "run.sh" -x "code_upload_challenge_evaluation/*" -x "remote_challenge_evaluation/*" -x "worker/*" -x "challenge_data/*" -x "github/*" -x ".github/*" -x "README.md"

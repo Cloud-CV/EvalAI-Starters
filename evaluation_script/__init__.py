bef235684c033c591f6e1f8cb49860fdc52cb54c
Harshit Jain <48647625+Harshit28j@users.noreply.github.com>
2025-03-18 10:17:40 +0530

    Custom Worker Packages: Add verbose output for pip install errors (#89)

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -13,8 +13,14 @@ def install(package):
 
     # Args:
     #     package ([str]): Package name with version
-    
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    try:
+        subprocess.run([sys.executable,"-m","pip","install",package])
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while installing {package}: {e.stderr}")
+    except FileNotFoundError:
+        print("Error: Pip is not found. make sure you have pip installed.")
+    except PermissionError:
+        print("Error: Permission denied. ")
 
 
 def install_local_package(folder_name):
@@ -23,15 +29,22 @@ def install_local_package(folder_name):
     # Args:
     #     folder_name ([str]): name of the folder placed in evaluation_script/
     
-    subprocess.check_output(
-    [
-        sys.executable,
-        "-m",
-        "pip",
-        "install",
-        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
-    ]
-)
+    
+    try:
+        subprocess.run([
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            os.path.join(str(Path(__file__).parent.absolute()), folder_name)
+        ], capture_output=True, text=True, check=True)
+        print(f"Successfully installed local package from {folder_name}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while installing local package from {folder_name}: {e.stderr}")
+    except FileNotFoundError:
+        print("Error: Pip not found.")
+    except PermissionError:
+        print("Error: Permission denied. ")
 
 install("shapely==1.7.1")
 install("requests==2.25.1")

d60c5ed10f0f372d57a7be093916b53e38c2127b
ZahedR_327 <f20213179@hyderabad.bits-pilani.ac.in>
2025-08-28 13:22:56 +0530

    Update config.py

--- github/config.py
+++ github/config.py
@@ -6,12 +6,12 @@ os.environ["CHALLENGE_ERRORS"] = "False"
 HOST_CONFIG_FILE_PATH = "github/host_config.json"
 CHALLENGE_CONFIG_VALIDATION_URL = "/api/challenges/challenge/challenge_host_team/{}/validate_challenge_config/"
 CHALLENGE_CREATE_OR_UPDATE_URL = "/api/challenges/challenge/challenge_host_team/{}/create_or_update_github_challenge/"
+GITHUB_SYNC_STATUS_URL = "/api/v1/challenges/{}/github/sync_status/"
 EVALAI_ERROR_CODES = [400, 401, 406]
 API_HOST_URL = "https://eval.ai"
 IGNORE_DIRS = [
     ".git",
     ".github",
-    "github",
     "code_upload_challenge_evaluation",
     "remote_challenge_evaluation",
 ]
@@ -24,5 +24,7 @@ IGNORE_FILES = [
 ]
 CHALLENGE_ZIP_FILE_PATH = "challenge_config.zip"
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
+# GitHub branch - use 'challenge' as the default branch
+GITHUB_BRANCH = os.getenv("GITHUB_REF_NAME") or os.getenv("GITHUB_BRANCH") or "challenge"
 GITHUB_EVENT_NAME = os.getenv("GITHUB_EVENT_NAME")
 VALIDATION_STEP = os.getenv("IS_VALIDATION")
